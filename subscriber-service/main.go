4c45d7956555d643b78aaa589ec28cf229b075aa
sammy <sammyoina@gmail.com>
2021-12-24 02:13:10 +0300

    Update main.go

--- subscriber-service/main.go
+++ subscriber-service/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	MQTT "github.com/eclipse/paho.mqtt.golang"
 
@@ -34,6 +36,11 @@ var f2 MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
 
 func main() {
 
+	MQTT.ERROR = log.New(os.Stdout, "[ERROR] ", 0)
+	MQTT.CRITICAL = log.New(os.Stdout, "[CRIT] ", 0)
+	MQTT.WARN = log.New(os.Stdout, "[WARN]  ", 0)
+	MQTT.DEBUG = log.New(os.Stdout, "[DEBUG] ", 0)
+
 	writeAPI, influxClient = controllers.InitializeInfluxDb()
 	defer controllers.CloseInfluxClient(writeAPI, influxClient)
 
@@ -44,6 +51,12 @@ func main() {
 	opts := MQTT.NewClientOptions().AddBroker("tcp://mosquitto:1883/")
 	opts.SetClientID("influx-pusher")
 	opts.SetDefaultPublishHandler(f)
+	opts.SetAutoReconnect(true)
+	opts.SetPingTimeout(time.Microsecond * 20)
+	//	opts.SetConnectTimeout(time.Minute * 10)
+	opts.SetKeepAlive(5 * time.Second)
+	opts.SetCleanSession(false)
+	opts.SetOrderMatters(false)
 	topic := "sensors"
 	topic2 := "health"
 
@@ -51,7 +64,7 @@ func main() {
 		if token := c.Subscribe(topic, 0, f); token.Wait() && token.Error() != nil {
 			panic(token.Error())
 		}
-		if token2 := c.Subscribe(topic2, 0, f2); token2.Wait() && token2.Error() != nil {
+		if token2 := c.Subscribe(topic2, 1, f2); token2.Wait() && token2.Error() != nil {
 			panic(token2.Error())
 		}
 	}
